ab6771068d3213e1a235e0987a9a8b9195f17569
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2021-04-15 16:12:45 +0000

    Adding bank when create character

--- src/main/java/com/fatec/sp/gov/br/gta/service/CharacterServiceImpl.java
+++ src/main/java/com/fatec/sp/gov/br/gta/service/CharacterServiceImpl.java
@@ -1,7 +1,10 @@
 package com.fatec.sp.gov.br.gta.service;
+import java.util.HashSet;
 
 import com.fatec.sp.gov.br.gta.entity.Character;
+import com.fatec.sp.gov.br.gta.entity.BankCharacter;
 import com.fatec.sp.gov.br.gta.repository.CharacterRepository;
+import com.fatec.sp.gov.br.gta.repository.BankCharacterRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,14 +16,20 @@ public class CharacterServiceImpl implements CharacterService {
 
     @Autowired 
     private CharacterRepository characterRepo;
-    
 
     @Override
     @Transactional
     public Character adicionarCharacter(String name) {
 
+        //bank
+        BankCharacter bank = new BankCharacter();
+        bank.setBalance(5000L);
+        
+        //character
         Character character = new Character();
         character.setName(name);
+        character.setBanks(new HashSet<BankCharacter>());
+        character.getBanks().add(bank);
         characterRepo.save(character);
         return character;
     }
