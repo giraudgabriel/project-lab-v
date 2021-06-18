262c12674bbc9e96a58b8a39a81cc72ad0b59da4
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2021-06-18 19:36:03 +0000

    fix roles

--- src/main/java/com/fatec/sp/gov/br/gta/controller/LoginController.java
+++ src/main/java/com/fatec/sp/gov/br/gta/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.fatec.sp.gov.br.gta.controller;
 
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fatec.sp.gov.br.gta.security.JwtUtils;
 import com.fatec.sp.gov.br.gta.security.Login;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +34,9 @@ public class LoginController {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+        login.setRoles(auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())
+                .toArray(new String[auth.getAuthorities().size()]));
+        login.setPassword(null);
         return login;
     }
 }
